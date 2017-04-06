217ec06efcc92113748b8ac450d6ce79978c4a92
rayGreycloud <rayGreycloud@gmail.com>
2017-04-06 13:34:02 -0700

    Add function to iterate thru channelsArray and create li for each

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// const channelsArray = ["freecodecamp", "brunofin", "comster404", "MaxguNtv", "chozilla", "VictoryForPhil", "Hazey7893", "NikolarnTV"];
+const channelsArray = ["freecodecamp", "brunofin", "comster404", "Pineaqples", "NikolarnTV"];
+let lis = [];
+let html = '';
 
-const channel = "freecodecamp";
-const cb = '?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm&callback=?';
-const url = 'https://api.twitch.tv/kraken/';
-const streamsUrl = `${url}streams/${channel}${cb}`;
+channelsArray.map(function(channel) {
+  const cb = '?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm&callback=?';
+  const url = 'https://api.twitch.tv/kraken/';
+  const streamsUrl = `${url}streams/${channel}${cb}`;
+  let results = {};
+  let status = '';
+  let thumbnail = "https://dummyimage.com/80x45/eeeeee/000000.jpg";
 
-let results = {};
-let status = '';
-let thumbnail = "https://dummyimage.com/80x45/eeeeee/000000.jpg";
+  $.getJSON(streamsUrl, function(data) {
+    results.data = data;
+    console.log(`Data for ${channel}`, results);
+  }).done(function(data) {
+    status = (data.stream !== null) ? 'Online' : 'Offline';
+    let li = `<li class="channel"><span><img src=${thumbnail}class="thumbnail"></span><span id="name"><a href="https://www.twitch.tv/${channel}" target="_blank">${channel}</a></span><span id="description">${status}</span></li>`;
+    lis.push(li);
 
-$.getJSON(streamsUrl, function(data) {
-  results.data = data;
-  console.log("CB response:", results);
+    $(li).appendTo("#channels");
+  });
 
-  status = (data.stream !== null) ? 'Online' : 'Offline';
-  let html = `<li class="channel">
-    <span><img src=${thumbnail}class="thumbnail"></span>
-    <span id="name"><a href="https://www.twitch.tv/${channel}" target="_blank">${channel}</a></span>
-    <span id="description">${status}</span>
-  </li>`;
-  document.querySelector('#channels').innerHTML = html;
 });
-
-console.log("Results at finish:", results);
+console.log("lis:", lis);
